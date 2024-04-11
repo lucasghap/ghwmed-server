cc48d0ad339b6b22015e99f359c1fb9f9938dc70
Lucas Ferreira <lucas.ferreira@ghap.com.br>
2024-04-10 22:07:24 -0300

    fix: auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
-import { PrismaService } from 'src/prisma.service'
+import { PrismaService } from 'src/prima.service'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { JwtStrategyService } from './jwt/jwt-strategy.service'
