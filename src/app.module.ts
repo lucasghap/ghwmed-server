2fd9002c1f98cd2c93988c132be381c491ddf0b1
Lucas Ferreira <lucas.ferreira@ghap.com.br>
2024-04-10 22:06:35 -0300

    feat: imports AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AttendancesModule } from './attendances/attendances.module';
+import { AuthModule } from './auth/auth.module';
 import { OracleModule } from './oracle/oracle.module';
 import { SchedulesModule } from './schedules/schedules.module';
-import { AttendancesModule } from './attendances/attendances.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [OracleModule, SchedulesModule, AttendancesModule, UsersModule],
+  imports: [OracleModule, SchedulesModule, AttendancesModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
