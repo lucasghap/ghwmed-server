12ee16f27da54825305dfa7885ffe3bef3a454a7
Lucas Ferreira <lucas.ferreira@ghap.com.br>
2024-04-12 12:21:50 -0300

    feat: sign user with providerId

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { compare } from 'bcryptjs'
 import { OracleService } from 'src/oracle/oracle.service'
 import { PrismaService } from 'src/prima.service'
 import { CreateAuthDto } from './dto/create-auth.dto'
+
+interface ProviderMv {
+  id: number 
+  cpf: string
+  name: string
+}
 
 @Injectable()
 export class AuthService {
@@ -14,19 +20,24 @@ export class AuthService {
   ) {}
 
   async create({ cpf, password, providerId }: CreateAuthDto) {
+    let providerMv: ProviderMv | null = null
+
     if (providerId) {
-      const provider = await this.oracle.query(`
+      const results = await this.oracle.query(`
         SELECT
           cd_prestador "id",
           nr_cpf_cgc "cpf",
           nm_prestador "name"
         FROM prestador 
         WHERE prestador.cd_prestador = 238
+        AND prestador.tp_situacao = 'A'
       `)
       
-      console.log(provider)
+      providerMv = results[0]
 
-      return
+      if (!providerMv) {
+        throw new NotFoundException('Prestador não foi encontrado na base do MV ou está inativo')
+      }
     }
 
     const cpfExists = await this.prisma.user.findUnique({
@@ -38,10 +49,12 @@ export class AuthService {
     if (!cpfExists)
       throw new UnauthorizedException('CPF ou senha inválidos')
 
-    const passwordMatch = await compare(password, cpfExists.password)
+    if (!providerId) {
+      const passwordMatch = await compare(password, cpfExists.password)
 
-    if (!passwordMatch)
-      throw new UnauthorizedException('CPF ou senha inválidos')
+      if (!passwordMatch)
+        throw new UnauthorizedException('CPF ou senha inválidos')
+    }
 
     const payload = {
       sub: cpfExists.id,
