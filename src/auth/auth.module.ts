cf16b2858478cf55140e69ea115c9cb744bfee42
Lucas Ferreira <lucas.ferreira@ghap.com.br>
2025-01-22 14:47:15 -0300

    fix: extend JWT expiration time to 1000 days

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,17 @@
-import { Module } from '@nestjs/common'
-import { JwtModule } from '@nestjs/jwt'
-import { OracleService } from 'src/oracle/oracle.service'
-import { PrismaService } from 'src/prima.service'
-import { AuthController } from './auth.controller'
-import { AuthService } from './auth.service'
-import { JwtStrategyService } from './jwt/jwt-strategy.service'
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { OracleService } from 'src/oracle/oracle.service';
+import { PrismaService } from 'src/prima.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategyService } from './jwt/jwt-strategy.service';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: {
-        expiresIn: '1 day',
+        expiresIn: '1000 day',
       },
     }),
   ],
