beacbb98e3d76fbb4b43c966e69d928aa8dbbfff
Lucas Ferreira <lucas.ferreira@ghap.com.br>
2025-01-22 09:38:07 -0300

    feat: add PrismaService to PasswordsModule providers

--- src/passwords/passwords.module.ts
+++ src/passwords/passwords.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmailsService } from 'src/emails/emails.service';
+import { PrismaService } from 'src/prima.service';
 import { PasswordsController } from './passwords.controller';
 import { PasswordsService } from './passwords.service';
 
 @Module({
   controllers: [PasswordsController],
-  providers: [PasswordsService, EmailsService],
+  providers: [PasswordsService, EmailsService, PrismaService],
 })
 export class PasswordsModule {}
