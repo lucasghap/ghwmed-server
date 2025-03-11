69f2891f07114a27ff727896a82a0a276d21316f
Lucas Ferreira <lucas.ferreira@ghap.com.br>
2025-03-11 14:10:13 -0300

    fix: improve SQL query in findAttendances method and handle null procedure values

--- src/attendances/attendances.service.ts
+++ src/attendances/attendances.service.ts
@@ -4,36 +4,38 @@ import { PrismaService } from 'src/prima.service';
 
 @Injectable()
 export class AttendancesService {
-  constructor(private oracle: OracleService, private prisma: PrismaService){}
+  constructor(private oracle: OracleService, private prisma: PrismaService) {}
 
   async findAttendances(userId: string) {
     const user = await this.prisma.user.findUnique({
       where: {
-        id: userId
-      }
-    })
+        id: userId,
+      },
+    });
 
-    const attendances = await this.oracle.query(`
+    const attendances = await this.oracle.query(
+      `
       SELECT
-      a.cd_atendimento "id"
-      ,c.nm_convenio "covenantName"
-      ,p.nm_paciente "patientName"
-      ,To_Char(p.dt_nascimento, 'dd/mm/yyyy') "birthDate"
-      ,fn_idade(p.dt_nascimento, 'a A m M d D') "age"
-      ,To_Char(a.dt_atendimento, 'dd/mm/yyyy') "date"
-      -- ,r.cd_prestador
-      -- ,r.nm_prestador
-      ,e.ds_especialid "specialtyName"
-      ,a.cd_pro_int "procedureId"
-      ,x.ds_pro_fat "procedureName"
-      ,u.ds_unid_int "unitName"
-      ,l.ds_leito "bedName"
-      ,Trunc(SYSDATE - a.dt_atendimento) "hospitalizedDays"
-        FROM atendime a, paciente p, prestador r, pro_fat x, convenio c, mov_int m, leito l, unid_int u, especialid e
+        a.cd_atendimento "id"
+        ,c.nm_convenio "covenantName"
+        ,p.nm_paciente "patientName"
+        ,To_Char(p.dt_nascimento, 'dd/mm/yyyy') "birthDate"
+        ,fn_idade(p.dt_nascimento, 'a A m M d D') "age"
+        ,To_Char(a.dt_atendimento, 'dd/mm/yyyy') "date"
+        -- ,r.cd_prestador
+        -- ,r.nm_prestador
+        ,e.ds_especialid "specialtyName"
+        ,Nvl(Nvl(a.cd_pro_int, a.cd_procedimento), 'Não informado') "procedureId"
+        ,Nvl(Nvl(x.ds_pro_fat, ps.ds_procedimento), 'Não informado') "procedureName"
+        ,u.ds_unid_int "unitName"
+        ,l.ds_leito "bedName"
+        ,Trunc(SYSDATE - a.dt_atendimento) "hospitalizedDays"   
+      FROM atendime a, paciente p, prestador r, pro_fat x, convenio c, mov_int m, leito l, unid_int u, especialid e, procedimento_sus ps
       WHERE a.cd_paciente = p.cd_paciente
         AND a.cd_prestador = r.cd_prestador
-        AND a.cd_pro_int = x.cd_pro_fat
-        AND a.cd_convenio = c.cd_convenio
+        AND a.cd_pro_int = x.cd_pro_fat(+)
+        AND a.cd_procedimento = ps.cd_procedimento(+) 
+        AND a.cd_convenio = c.cd_convenio                      
         AND a.cd_atendimento = m.cd_atendimento
         AND m.cd_leito = l.cd_leito
         AND l.cd_unid_int = u.cd_unid_int
@@ -43,10 +45,12 @@ export class AttendancesService {
         AND a.tp_atendimento = 'I'
         AND r.NR_CPF_CGC  = :cpf
       ORDER BY a.dt_atendimento
-    `, {
-      cpf: user.cpf
-    })
+    `,
+      {
+        cpf: user.cpf,
+      },
+    );
 
-    return attendances
+    return attendances;
   }
 }
